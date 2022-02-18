0a48be25a8dcc6c11d5cce499d135df5e0192f4b
Astha Ramwala <astha.ramwala@gmail.com>
2022-02-18 12:48:30 +0530

    TDD: failing test case - string calculator should return correct value for string one

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,5 +1,14 @@
 from calc.string_calculator import add
 
+# TDD CYCLE
+# 1. MAKE TEST THAT FAILS
+# 2. MAKE THE TEST PASS
+# 3. REFACTOR
+
 def test_string_calc_should_return_zero_on_empty_string():
     result = add("")
     assert result == 0, "String calculater should return 0 on empty string"
+
+def test_string_calc_should_return_correct_value_on_one():
+    result = add("1")
+    assert result == 1, "String calculater should return 1 on \"1\" string"
